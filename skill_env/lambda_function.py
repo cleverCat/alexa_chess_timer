56f06af7e9bf1664e3f4b14602a3c31751b85c6d
cleverCat <igor.sotona@gmail.com>
2019-02-25 21:43:06 +0300

    clear style

--- skill_env/lambda_function.py
+++ skill_env/lambda_function.py
@@ -15,7 +15,6 @@ from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
 from chess_timer import ChessTimer
 
-
 # =========================================================================================================================================
 # TODO: The items below this comment need your attention.
 # =========================================================================================================================================
@@ -28,29 +27,17 @@ FALLBACK_REPROMPT = "What can I help you with?" + HELP_REPROMPT
 EXCEPTION_MESSAGE = "Sorry. I cannot help you with that."
 STEP_MESSAGE = 'It\'s {0} turn,'
 JOKE_LIST = [
-    'May the Force be with {0}',
-    'Stay awhile and listen',
-    'Hold his beer',
-    'As the prophecy foretold',
-    'It is super effective',
-    'Party is over',
+    'May the Force be with {0}', 'Stay awhile and listen', 'Hold his beer',
+    'As the prophecy foretold', 'It is super effective', 'Party is over',
     'Brace yourselves, {0} is coming',
     'I could calculate your chance of survival, {0}, but you will not like it”',
-    'Release the Kraken',
-    'Just as planned',
-    'Oh what a day. What a lovely day',
-    'Look at {0}. He is the captain now',
-    'Are you not entertained?',
-    'May the odds ever be in your favor',
-    'Why so serious, {0}?',    
-    'Just keep swimming, {0}',
-    'You are a wizard, {0}',
-    'Here is {0}!',
-    'A wild {0} appeared',
-    'The only way to win is not to play, {0}',
-    'Do you feel in charge, {0}?'
-    ]
-
+    'Release the Kraken', 'Just as planned',
+    'Oh what a day. What a lovely day', 'Look at {0}. He is the captain now',
+    'Are you not entertained?', 'May the odds ever be in your favor',
+    'Why so serious, {0}?', 'Just keep swimming, {0}', 'You are a wizard, {0}',
+    'Here is {0}!', 'A wild {0} appeared',
+    'The only way to win is not to play, {0}', 'Do you feel in charge, {0}?'
+]
 
 # =========================================================================================================================================
 # Editing anything below this line might break your skill.
@@ -63,14 +50,20 @@ logger.setLevel(logging.DEBUG)
 
 def get_statistic(session):
     chess_timer = ChessTimer(session)
+
     def get_statistic_time(_time):
         from datetime import timedelta
         current_time_delta = timedelta(seconds=_time)
         current_minutes = current_time_delta.seconds // 60 % 60
-        current_seconds = current_time_delta.seconds - current_minutes * 60 
-        return 'minutes ' + str(current_minutes) + ' seconds ' + str(current_seconds)
+        current_seconds = current_time_delta.seconds - current_minutes * 60
+        return 'minutes ' + str(current_minutes) + ' seconds ' + str(
+            current_seconds)
+
     statistic = chess_timer.get_statistic()
-    list_statistic = [str(player) + ' ' + get_statistic_time(_time) + ' ' for player, _time in statistic.items()]
+    list_statistic = [
+        str(player) + ' ' + get_statistic_time(_time) + ' '
+        for player, _time in statistic.items()
+    ]
     speech = ' '.join(list_statistic)
     return speech
 
@@ -78,15 +71,15 @@ def get_statistic(session):
 class StartHandle(AbstractRequestHandler):
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return is_request_type("LaunchRequest")(handler_input) 
+        return is_request_type("LaunchRequest")(handler_input)
+
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         logger.info("In GetStartHandler")
-        session = handler_input.attributes_manager.session_attributes
-        chess_timer = ChessTimer(session)
         reprompt = "Say yes to start the game or no to quit."
         handler_input.response_builder.speak(HELP_MESSAGE).ask(reprompt)
         return handler_input.response_builder.response
+
 
 class GetStatisticHandle(AbstractRequestHandler):
     def can_handle(self, handler_input):
@@ -101,7 +94,7 @@ class GetStatisticHandle(AbstractRequestHandler):
         handler_input.response_builder.speak(speech).ask('help text')
         return handler_input.response_builder.response
 
-       
+
 class GetStepHandle(AbstractRequestHandler):
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
@@ -112,20 +105,22 @@ class GetStepHandle(AbstractRequestHandler):
         logger.info("In GetStepHandle")
         session = handler_input.attributes_manager.session_attributes
         chess_timer = ChessTimer(session)
-       
+
         new_session = chess_timer.step()
 
         handler_input.attributes_manager.session_attributes = new_session
         logger.info(new_session['current_player_name'])
-         
+
         logger.info(STEP_MESSAGE + ' ' + random.choice(JOKE_LIST))
-        handler_input.response_builder.speak((STEP_MESSAGE + ' ' + random.choice(JOKE_LIST)).format(new_session['current_player_name'])).ask('hello')
+        handler_input.response_builder.speak(
+            (STEP_MESSAGE + ' ' + random.choice(JOKE_LIST)).format(
+                new_session['current_player_name'])).ask('hello')
         return handler_input.response_builder.response
-       
 
 
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return is_intent_name("AMAZON.HelpIntent")(handler_input)
@@ -135,17 +130,17 @@ class HelpIntentHandler(AbstractRequestHandler):
         logger.info("In HelpIntentHandler")
 
         handler_input.response_builder.speak(HELP_MESSAGE).ask(
-            HELP_REPROMPT).set_card(SimpleCard(
-                SKILL_NAME, HELP_MESSAGE))
+            HELP_REPROMPT).set_card(SimpleCard(SKILL_NAME, HELP_MESSAGE))
         return handler_input.response_builder.response
 
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
     """Single handler for Cancel and Stop Intent."""
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                is_intent_name("AMAZON.StopIntent")(handler_input))
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input)
+                or is_intent_name("AMAZON.StopIntent")(handler_input))
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
@@ -164,6 +159,7 @@ class FallbackIntentHandler(AbstractRequestHandler):
     This handler will not be triggered except in that locale,
     so it is safe to deploy on any locale.
     """
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return is_intent_name("AMAZON.FallbackIntent")(handler_input)
@@ -179,6 +175,7 @@ class FallbackIntentHandler(AbstractRequestHandler):
 
 class SessionEndedRequestHandler(AbstractRequestHandler):
     """Handler for Session End."""
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return is_request_type("SessionEndedRequest")(handler_input)
@@ -197,6 +194,7 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
     """Catch all exception handler, log exception and
     respond with custom message.
     """
+
     def can_handle(self, handler_input, exception):
         # type: (HandlerInput, Exception) -> bool
         return True
@@ -215,6 +213,7 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 # Request and Response loggers
 class RequestLogger(AbstractRequestInterceptor):
     """Log the alexa requests."""
+
     def process(self, handler_input):
         # type: (HandlerInput) -> None
         logger.debug("Alexa Request: {}".format(
@@ -223,6 +222,7 @@ class RequestLogger(AbstractRequestInterceptor):
 
 class ResponseLogger(AbstractResponseInterceptor):
     """Log the alexa responses."""
+
     def process(self, handler_input, response):
         # type: (HandlerInput, Response) -> None
         logger.debug("Alexa Response: {}".format(response))
--- skill_env/test/test_chess_timer.py
+++ skill_env/test/test_chess_timer.py
@@ -2,28 +2,30 @@ import unittest
 from mock import patch
 
 
-
 class TestChessTimer(unittest.TestCase):
-
     def setUp(self):
         import sys
-        if 'chess_timer' in sys.modules.keys(): 
+        if 'chess_timer' in sys.modules.keys():
             del sys.modules['chess_timer']
         self._list_module = list(sys.modules.keys())
 
     def tearDown(self):
         import sys
         current_list_module = list(sys.modules.keys())
-        for i in [i for i in current_list_module if i not in self._list_module]:
+        for i in [
+                i for i in current_list_module if i not in self._list_module
+        ]:
             del sys.modules[i]
         if 'chess_timer' in sys.modules.keys():
             del sys.modules['dhess_timer']
 
-
     def test_init(self):
         from chess_timer import ChessTimer
         from datetime import timedelta
-        init_state = {player:timedelta(microseconds=0).microseconds for player in ChessTimer._players}
+        init_state = {
+            player: timedelta(microseconds=0).microseconds
+            for player in ChessTimer._players
+        }
         init_state['current_player'] = None
         init_state['current_player_name'] = None
         init_state['start_current_step_time'] = None
@@ -32,7 +34,6 @@ class TestChessTimer(unittest.TestCase):
     def test_init_step(self):
 
         from datetime import datetime
-        from datetime import timedelta
         current_time = datetime.now()
         with patch('datetime.datetime') as mock_datetime:
             mock_datetime.now.return_value = current_time
@@ -50,20 +51,19 @@ class TestChessTimer(unittest.TestCase):
             from chess_timer import ChessTimer
             state = ChessTimer(
                 dict(
-                    current_player=len(ChessTimer._players)-1,
-                    start_current_step_time = current_time.isoformat()
-                )
-            ).step()
+                    current_player=len(ChessTimer._players) - 1,
+                    start_current_step_time=current_time.isoformat())).step()
             self.assertEqual(state['current_player'], 0)
-    
+
     def test_step(self):
 
         from datetime import datetime
         from datetime import timedelta
+
         def _get_time(dt_str):
-            dt, _, us= dt_str.partition(".")
+            dt, _, us = dt_str.partition(".")
             dt = datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
-            return dt 
+            return dt
 
         current_time = _get_time(datetime.now().isoformat())
         with patch('datetime.datetime') as mock_datetime:
@@ -71,19 +71,22 @@ class TestChessTimer(unittest.TestCase):
             old_time = current_time - timedelta(minutes=10)
             mock_datetime.strptime.return_value = old_time
             from chess_timer import ChessTimer
-            state = ChessTimer(dict(
-                current_player=0,
-                start_current_step_time=(old_time).isoformat()
-                )
-            ).step()
+            state = ChessTimer(
+                dict(
+                    current_player=0,
+                    start_current_step_time=(old_time).isoformat())).step()
             self.assertEqual(state['current_player'], 1)
-            self.assertEqual(state[ChessTimer._players[0]], (current_time-_get_time(old_time.isoformat())).total_seconds())
+            self.assertEqual(state[ChessTimer._players[0]],
+                             (current_time - _get_time(
+                                 old_time.isoformat())).total_seconds())
 
     def test_get_statistic(self):
         from datetime import timedelta
         from chess_timer import ChessTimer
-        state = ChessTimer(dict(Red=timedelta(minutes=10).total_seconds())).get_statistic()
+        state = ChessTimer(
+            dict(Red=timedelta(minutes=10).total_seconds())).get_statistic()
         self.assertEqual(state['Red'] // 60 % 60, 10)
+
 
 if __name__ == '__main__':
     unittest.main()
